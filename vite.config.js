365b8b83418c5842661bc59a832440f9bd51a41e
itVpl <it@vpower-logistics.com>
2025-10-17 00:19:05 +0530

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'     // ⬅️ v4 plugin
 
 export default defineConfig({
-  plugins: [react(), tailwind()]             // ⬅️ add here
+  plugins: [react(), tailwind()],            // ⬅️ add here
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['leaflet', 'react-leaflet']
+  }
 })
